9b254a970197d1c6df1d74161f2300db43e3c4c3
lucasnpc <lucasnpc2@outlook.com>
2022-08-23 19:48:35 -0300

    Add maps api key and google services

--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,31 +1,29 @@
 package com.udacity.project4.locationreminders
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.navigation.fragment.NavHostFragment
-import com.udacity.project4.R
-import kotlinx.android.synthetic.main.activity_reminders.*
+import androidx.navigation.Navigation
+import com.udacity.project4.databinding.ActivityRemindersBinding
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
 
+    private val binding: ActivityRemindersBinding by lazy {
+        ActivityRemindersBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
-
+        setContentView(binding.root)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                Navigation.findNavController(binding.navHostFragment).popBackStack()
                 return true
             }
         }
