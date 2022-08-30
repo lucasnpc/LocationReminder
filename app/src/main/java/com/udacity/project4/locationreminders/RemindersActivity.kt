2827f77f5ab53cd03393b120f581e5bc6051924b
lucasnpc <lucasnpc2@outlook.com>
2022-08-30 19:25:15 -0300

    Fix permission request

--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -2,19 +2,21 @@ package com.udacity.project4.locationreminders
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.udacity.project4.R
 import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.databinding.ActivityRemindersBinding
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.utils.AuthenticationState
-import com.udacity.project4.utils.LOCATION_PERMISSION_REQUEST_CODE
-import com.udacity.project4.utils.PermissionUtils.isPermissionGranted
-import com.udacity.project4.utils.enableMyLocation
 import com.udacity.project4.utils.permissionDenied
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -28,10 +30,49 @@ class RemindersActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissio
         binding.navHostFragment.findNavController()
     }
 
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    backgroundLocationPermissionRequest.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                }
+            }
+            permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    backgroundLocationPermissionRequest.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                }
+            }
+            else -> {
+                permissionDenied = true
+                Snackbar.make(
+                    binding.reminderMain,
+                    R.string.permission_denied_explanation,
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+    private val backgroundLocationPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted)
+                println("Background Permission received")
+            else {
+                permissionDenied = true
+                Snackbar.make(
+                    binding.reminderMain,
+                    R.string.background_permission_denied_explanation,
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         observeAuthenticationState()
+        requestForegroundAndBackgroundLocationPermissions()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -51,36 +92,38 @@ class RemindersActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissio
                     startActivity(Intent(this, AuthenticationActivity::class.java))
                     finish()
                 }
-                else -> {
-                    println("else authenticated")
-                }
+                else -> Unit
             }
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
-            super.onRequestPermissionsResult(
-                requestCode,
-                permissions,
-                grantResults
-            )
-            return
-        }
+    private fun foregroundAndBackgroundLocationPermissionApproved(): Boolean {
+        val foregroundLocationApproved = (
+                PackageManager.PERMISSION_GRANTED ==
+                        ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ))
+        val backgroundPermissionApproved =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                PackageManager.PERMISSION_GRANTED ==
+                        ActivityCompat.checkSelfPermission(
+                            this, Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                        )
+            else
+                true
 
-        if (isPermissionGranted(
-                permissions,
-                grantResults,
-                Manifest.permission.ACCESS_FINE_LOCATION
+        return foregroundLocationApproved && backgroundPermissionApproved
+    }
+
+    private fun requestForegroundAndBackgroundLocationPermissions() {
+        if (foregroundAndBackgroundLocationPermissionApproved())
+            return
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
             )
-        ) {
-            enableMyLocation()
-        } else {
-            permissionDenied = true
-        }
+        )
     }
 }
